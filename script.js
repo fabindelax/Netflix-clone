6bd3c10629a0ee9623a2139c589dcec401560c3d
Fabio Augusto <113560771+fabindelax@users.noreply.github.com>
2024-04-30 17:50:50 -0300

    correção

--- script.js
+++ script.js
@@ -1,66 +1,67 @@
-function handler(e) {
-    e.preventDefault();
+window.onload = () => {
+    const form = document.querySelector('.form');
+    form.addEventListener('submit', searchMovies);
 
-    let movie = document.querySelector('.form__input').value;
+    // Carregar filmes ao carregar a página
+    loadMovies();
+};
 
-    if(movie) {
-
-        const _url = `http://www.omdbapi.com/?s=${movie}&apikey=b58710e2`;
-        const _options = {
-            method: 'GET',
-            mode: 'cors',
-            redirect: 'follow',
-            cache: 'default'
-        }
-
-
-        fetch(_url, _options) 
-            .then(function(response) {
-
-                // Tratamento de erro
-                if(!response.ok) throw new Error('Erro ao executar requisição');
-
-                // Retorne um objeto no formato json
-                return response.json();
-
-            })
-            // receber dados
-            .then(function(data) {
-                //console.log(data);
-
-                let newContent = '';
-                for(let i = 1; i < data.Search.length; i++) {
-                    newContent += `<li class="app-movies-all__card">`;
-                    newContent += `<figure class="app-movies-all__figure">`;
-                    newContent += `<img class="app-movies-all__thumb" src="${data.Search[i].Poster}"/>`;
-                    newContent += `</figure>`;
-                    newContent += `<legend class="app-movies-all__legend">`;
-                    newContent += `<span class="app-movies-all__year">${data.Search[i].Year}</span>`;
-                    newContent += `<h2 class="app-movies-all__title">${data.Search[i].Title}</h2>`;
-                    newContent += `</legend>`;
-                    newContent += `</li>`;
-                }
-
-                document.getElementById('movies').innerHTML = newContent;
-
-            })
-
-
-
-    } else {
-        alert('Digite um nome de filme!');
-    }
-
+function loadMovies() {
+    fetch("filmes.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => {
+            showMovies(data);
+        })
+        .catch((error) => {
+            console.error("Unable to fetch data:", error);
+            alert('Erro ao buscar filmes.');
+        });
 }
 
+function searchMovies(event) {
+    event.preventDefault();
 
+    const searchTerm = document.querySelector('.form__input').value.trim().toLowerCase();
 
+    fetch("filmes.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => {
+            const filteredMovies = data.filter(movie =>
+                movie.titulo.toLowerCase().includes(searchTerm)
+            );
+            showMovies(filteredMovies);
+        })
+        .catch((error) => {
+            console.error("Unable to fetch data:", error);
+            alert('Erro ao buscar filmes.');
+        });
+}
 
+function showMovies(movies) {
+    let moviesList = document.getElementById("movies");
+    let newContent = '';
 
+    movies.forEach((movie) => {
+        newContent += `<li class="app-movies-all__card">`;
+        newContent += `<figure class="app-movies-all__figure">`;
+        newContent += `<img class="app-movies-all__thumb" src="${movie.poster}"/>`;
+        newContent += `</figure>`;
+        newContent += `<legend class="app-movies-all__legend">`;
+        newContent += `<span class="app-movies-all__year">${movie.ano}</span>`;
+        newContent += `<h2 class="app-movies-all__title">${movie.titulo}</h2>`;
+        newContent += `</legend>`;
+        newContent += `</li>`;
+    });
 
-
-window.onload = () => {
-
-    const submit = document.querySelector('.form__submit');
-    submit.addEventListener('click', handler);
+    moviesList.innerHTML = newContent;
 }
